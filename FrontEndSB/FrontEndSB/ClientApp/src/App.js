064eacd0489d99b834975003d97abb1b6af82deb
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-01-30 12:28:31 +0200

    Default, Defined and Undefined Routes Completed

--- FrontEndSB/FrontEndSB/ClientApp/src/App.js
+++ FrontEndSB/FrontEndSB/ClientApp/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import New from './components/New';
+
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <Layout>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/counter' component={Counter} />
+            <Route exact path='/fetch-data/:startDateIndex?' component={FetchData} />
+            <Route exact path='/new' component={New} />
+            <Redirect from='/' to="/" />
+        </Switch>
+    </Layout>
 );
+
+
+
+
